9d2e4d4caa4cd3604f4c47f6f47322dbe9ee9cf2
danillolec@gmail.com <dUg3wdmw>
2022-10-10 22:19:23 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error(){
+    }
+
+    public void printInfo() {
+        System.out.println("Ошибка: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Пояснение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 1, "Ошибка ввода параметров");
+        error1.printInfo();
+        System.out.println();
+        error2.printInfo();
+    }
+}
