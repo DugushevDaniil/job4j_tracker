cbe37c873f385e44847a487979f276b1b61423da
danillolec@gmail.com <dUg3wdmw>
2022-10-27 17:55:14 +0300

    1.8. Состояние объекта [#167388 #352669]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -1,6 +1,7 @@
 package ru.job4j.oop;
 
 public class Battery {
+
     private int load;
 
     public Battery(int load) {
@@ -10,6 +11,7 @@ public class Battery {
     public int getLoad() {
         return load;
     }
+
 
     public void exchange(Battery another) {
         another.load = this.load + another.load;
