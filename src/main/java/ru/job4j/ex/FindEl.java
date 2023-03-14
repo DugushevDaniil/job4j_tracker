9232ba686ca7a0e2bc8ab6c6e6d800039ab70b52
danillolec@gmail.com <dUg3wdmw>
2023-03-14 15:34:46 +0300

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,14 +2,15 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        if (key == null) {
-            throw new ElementNotFoundException("Элемент массива не может быть null");
-        }
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Элемент массива не может быть null");
         }
         return rsl;
     }
