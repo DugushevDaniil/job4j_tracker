da2b2615ed1bd584e012e9f9bfecca1ab042891d
danillolec@gmail.com <dUg3wdmw>
2023-05-15 20:07:18 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,10 +5,10 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("test1@gmail.com", "Ivanov Ivan Ivanovich");
+        map.put("test@gmail.com", "Ivanov Ivan Ivanovich");
         map.put("test2@gmail.com", "Sidorov Ivan Ivanovich");
         map.put("test3@gmail.com", "Malov Ivan Ivanovich");
-        map.put("test1@gmail.com", "Petrov Ivan Ivanovich");
+        map.put("test@gmail.com", "Petrov Ivan Ivanovich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
