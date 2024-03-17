1ab8080cd5fb9cd56c056a41375d07b4e995b03e
danillolec@gmail.com <dUg3wdmw>
2024-03-17 23:14:18 +0300

    3. Отладка лямбда в IDEA [#259165]

--- src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
+++ src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class FilterNegativeNumbers {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, -5, -4, 6, -7, 4,-10, 5, -1);
+        List<Integer> numbers = Arrays.asList(1, -5, -4, 6, -7, 4, -10, 5, -1);
         List<Integer> positive = numbers.stream().filter(number -> number > 0
         ).collect(Collectors.toList());
         positive.forEach(System.out::println);
